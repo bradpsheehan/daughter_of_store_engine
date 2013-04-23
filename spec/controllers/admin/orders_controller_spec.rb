c64dde5e97d78ee513a59451c5d5c3a54557f29a
Christopher <ChrisKnight.mail@gmail.com>
2013-04-23 15:27:35 -0600

    admin order controller spec fixed

--- spec/controllers/admin/orders_controller_spec.rb
+++ spec/controllers/admin/orders_controller_spec.rb
@@ -1,40 +1,27 @@
 require 'spec_helper'
 
 describe Admin::OrdersController do
-  it "index action should render index template" do
-    user = FactoryGirl.create(:uber)
+   let(:user) {FactoryGirl.create(:uber)}
+   let(:store) {FactoryGirl.create(:store)}
+   let!(:order) {FactoryGirl.create(:order, store: store, user: user)}
+ before do
     controller.stub(:require_admin => true)
     controller.stub(:current_user => user)
-
-    get :index
-    response.should render_template(:index)
-  end
+    controller.stub(:current_store => store)
+ end
 
   it "index action should return a collection of orders" do
-    user = FactoryGirl.create(:uber)
-    orders = [FactoryGirl.create(:order, user: user)]
-    controller.stub(:require_admin => true)
-    controller.stub(:current_user => user)
-
     get :index
-    expect(assigns(:orders)).to match_array orders
+    expect(assigns(:orders)).to match_array [order]
   end
 
   it "show action should return an individual order" do
-    user = FactoryGirl.create(:uber)
-    order = FactoryGirl.create(:order, user: user)
-    controller.stub(:require_admin => true)
-    controller.stub(:current_user => user)
     get :show, id: order.id
     expect(assigns(:order)).to eq order
   end
 
   describe 'update' do
     it 'works correctly' do
-      user = FactoryGirl.create(:uber)
-      order = FactoryGirl.create(:order, user_id: user.id)
-      controller.stub(:require_admin => true)
-      controller.stub(:current_user => user)
       request.env["HTTP_REFERER"] = '/'
       post :update, id: order.id, update_status: 'pending'
       expect(order.status).to eq 'pending'
