4052723e760f0926c8c56ae0bca5974c59ed3433
Bradley Sheehan <bradpsheehan@gmail.com>
2013-04-23 19:12:31 -0600

    more refactoring and added test to cover orphan user completing registration with email

--- spec/features/anon_user_creates_account_spec.rb
+++ spec/features/anon_user_creates_account_spec.rb
@@ -1,63 +1,74 @@
 require 'spec_helper'
 
 describe 'new user creates and edits account' do
+
   describe 'registering a new account' do
+    it 'creates a new user account with unique login info' do
+      Resque.stub(:enqueue).with(WelcomeEmailJob, "poetry@poetry.com", "Maya Angelou") { true }
 
-    context 'when they provide unique login info' do
-      it 'creates a new user account' do
-        Resque.stub(:enqueue).with(WelcomeEmailJob, "poetry@poetry.com", "Maya Angelou") { true }
+      visit signup_path
+      fill_in "Full Name", with: 'Maya Angelou'
+      fill_in "Email", with: "poetry@poetry.com"
+      fill_in "Display Name", with: 'poet'
+      fill_in "user_password", with: 'poet'
+      fill_in "user_password_confirmation", with: 'poet'
+      click_button "Submit"
 
-        visit signup_path
-        fill_in "Full Name", with: 'Maya Angelou'
-        fill_in "Email", with: "poetry@poetry.com"
-        fill_in "Display Name", with: 'poet'
-        fill_in "user_password", with: 'poet'
-        fill_in "user_password_confirmation", with: 'poet'
-        click_button "Submit"
-
-        expect(page).to have_content "Welcome, Maya Angelou"
-        expect(current_path).to eq root_path
-      end
+      expect(page).to have_content "Welcome, Maya Angelou"
+      expect(current_path).to eq root_path
     end
 
-    context 'when they provide non-unique login info for registration' do
-      it 'returns an error message' do
-        email = "poetry@poetry.com"
-        FactoryGirl.create(:user, email: email)
+    it 'returns an error message when the email already has an account' do
+      email = "poetry@poetry.com"
+      FactoryGirl.create(:user, email: email)
 
-        visit signup_path
-        fill_in "Full Name", with: 'NOT MAYA'
-        fill_in "Email", with: email
-        fill_in "Display Name", with: 'poet'
-        fill_in "user_password", with: 'poet'
-        fill_in "user_password_confirmation", with: 'poet'
-        click_button "Submit"
+      visit signup_path
+      fill_in "Full Name", with: 'NOT MAYA'
+      fill_in "Email", with: email
+      fill_in "Display Name", with: 'poet'
+      fill_in "user_password", with: 'poet'
+      fill_in "user_password_confirmation", with: 'poet'
+      click_button "Submit"
 
-        expect(page).to have_content "Emailhas already been taken"
-        expect(current_path).to eq '/users'
-      end
+      expect(page).to have_content "Emailhas already been taken"
+      expect(current_path).to eq '/users'
     end
 
+    it "finishes registration for an orphan user" do
+      user = User.new_guest(email: "maya@example.com")
+      user.save!
+
+      visit signup_path
+
+      fill_in "Full Name", with: 'Maya Angelou'
+      fill_in "Email", with: user.email
+      fill_in "Display Name", with: 'poet'
+      fill_in "user_password", with: 'poet'
+      fill_in "user_password_confirmation", with: 'poet'
+      click_button "Submit"
+
+      user.reload
+      expect(user.full_name).to eq 'Maya Angelou'
+      expect(user).to_not be_orphan
+    end
   end
 
-  describe 'signing in and out of account' do
-    it 'allows them to log out of their account' do
-      user = FactoryGirl.create(:user, password: 'test123', password_confirmation: 'test123')
+  it 'allows them to log out of their account' do
+    user = FactoryGirl.create(:user, password: 'test123', password_confirmation: 'test123')
 
-      login(user.email, 'test123')
+    login(user.email, 'test123')
 
-      expect(current_path).to eq root_path
-      click_link_or_button "Logout"
-      expect(current_path).to eq root_path
-      expect(page).to have_content "Logged out!"
-    end
+    expect(current_path).to eq root_path
+    click_link_or_button "Logout"
+    expect(current_path).to eq root_path
+    expect(page).to have_content "Logged out!"
+  end
 
-    it 'rejects incorrect login info on signin' do
-      login('user@example.com', 'test345')
+  it 'rejects incorrect login info on signin' do
+    login('user@example.com', 'test345')
 
-      expect(current_path).to eq login_path
-      expect(page).to have_content 'invalid'
-    end
+    expect(current_path).to eq login_path
+    expect(page).to have_content 'invalid'
   end
 
   it 'edits the account with valid input' do
