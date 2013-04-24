26fd693090d020e1fc3e038f8055c7e2e001fbcb
Bradley Sheehan <bradpsheehan@gmail.com>
2013-04-23 18:03:29 -0600

    refactor anon user creates account spec

--- spec/features/anon_user_creates_account_spec.rb
+++ spec/features/anon_user_creates_account_spec.rb
@@ -1,24 +1,20 @@
 require 'spec_helper'
 
 describe 'new user creates and edits account' do
-  def signup_user(email = "poetry@poetry.com")
-    visit signup_path
-    fill_in "Full Name", with: 'Maya Angelou'
-    fill_in "Email", with: email
-    fill_in "Display Name", with: 'poet'
-    fill_in "user_password", with: 'poet'
-    fill_in "user_password_confirmation", with: 'poet'
-    click_button "Submit"
-  end
-
   describe 'registering a new account' do
-    before do
-      Resque.stub(:enqueue).with(WelcomeEmailJob, "poetry@poetry.com", "Maya Angelou") { true }
-    end
 
     context 'when they provide unique login info' do
       it 'creates a new user account' do
-        signup_user
+        Resque.stub(:enqueue).with(WelcomeEmailJob, "poetry@poetry.com", "Maya Angelou") { true }
+
+        visit signup_path
+        fill_in "Full Name", with: 'Maya Angelou'
+        fill_in "Email", with: "poetry@poetry.com"
+        fill_in "Display Name", with: 'poet'
+        fill_in "user_password", with: 'poet'
+        fill_in "user_password_confirmation", with: 'poet'
+        click_button "Submit"
+
         expect(page).to have_content "Welcome, Maya Angelou"
         expect(current_path).to eq root_path
       end
@@ -45,15 +41,11 @@ describe 'new user creates and edits account' do
   end
 
   describe 'signing in and out of account' do
-    before(:each) do
-      signup_user
-    end
-
     it 'allows them to log out of their account' do
-      visit login_path
-      fill_in "Email", with: 'poetry@poetry.com'
-      fill_in "Password", with: "poet"
-      click_button "Login"
+      user = FactoryGirl.create(:user, password: 'test123', password_confirmation: 'test123')
+
+      login(user.email, 'test123')
+
       expect(current_path).to eq root_path
       click_link_or_button "Logout"
       expect(current_path).to eq root_path
@@ -61,10 +53,8 @@ describe 'new user creates and edits account' do
     end
 
     it 'rejects incorrect login info on signin' do
-      visit login_path
-      fill_in "Email", with: 'poetry@poetry.com'
-      fill_in "Password", with: "whatsmypassword"
-      click_button "Login"
+      login('user@example.com', 'test345')
+
       expect(current_path).to eq login_path
       expect(page).to have_content 'invalid'
     end
@@ -73,15 +63,19 @@ describe 'new user creates and edits account' do
   it 'edits the account with valid input' do
     user = FactoryGirl.create(:user, password: 'test123', password_confirmation: 'test123')
 
-    visit login_path
-    fill_in "Email", with: user.email
-    fill_in "Password", with: 'test123'
-    click_button "Login"
+    login(user.email, 'test123')
 
     visit profile_path
     fill_in "Display Name", with: 'Maya'
     click_button "Submit"
     expect(current_path).to eq profile_path
     expect(page).to have_content "updated account"
+  end
+
+  def login(email, password)
+    visit login_path
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    click_button "Login"
   end
 end
