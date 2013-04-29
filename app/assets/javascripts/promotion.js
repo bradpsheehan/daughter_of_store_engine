d30262404d3def161c4dcf246769b6e3d7192c6e
Dan Mee <danmee10@gmail.com>
2013-04-28 18:35:10 -0600

    existing promotions show up when admin enters product edit page, promotional price shown and updated as admin moves slide bar.  fixed promo-price method in product model so it no longer sets the product's price

--- app/assets/javascripts/promotion.js
+++ app/assets/javascripts/promotion.js
@@ -1,12 +1,33 @@
 $(document).ready(function() {
-    $( "#slider-range-min" ).slider({
-      range: "min",
-      value: 0,
-      min: 0,
-      max: 100,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value );
-      }
-    });
-    $( "#amount" ).val($( "#slider-range-min" ).slider( "value" ) + "%");
+  var $existingPromotionValue = $('#existing-promotion-value').text();   //hackity-hack
+  // var $fullPrice = $('#full-price').text();
+  $('#existing-promotion-value').css('opacity', 0);                     //hackity-hack
+  $('#full-price').css('opacity', 0);                                     //hackity-hack
+
+  $( "#slider-range-min" ).slider({
+    range: "min",
+    value: $existingPromotionValue,
+    min: 0,
+    max: 100,
+    slide: function( event, ui ) {
+      $( "#amount" ).val( ui.value );
+      var $sliderValue = +$('#amount').val();
+      var $fullPrice = +$('#full-price').text();
+      $('#promotion-price').text('Promotional Price:' + ($fullPrice - (($sliderValue/100) * $fullPrice)));
+    }
   });
+  $( "#amount" ).val($( "#slider-range-min" ).slider( "value" ));
+
+
+  var $fullPrice = +$('#full-price').text();
+  $('#promotion-price').text('Promotional Price:' + ($fullPrice - ((+$existingPromotionValue/100) * $fullPrice)));
+  $('#amount').keyup(function() {
+    var $newPromotionValue = +$(this).val();
+    $('#promotion-price').text(+$fullPrice - +(($newPromotionValue/100) * $fullPrice));
+  });
+});
+// ($newPromotionValue/100) * $fullPrice
+
+//1) make an element in the form with the value and call text here and set value to that...
+        //problem-- can't get @product.promotion data to javascript file
+
