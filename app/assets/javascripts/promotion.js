931fa96b71d9fe2cc553302cc91ad56ddb5acca4
Dan Mee <danmee10@gmail.com>
2013-04-29 11:02:04 -0600

    small formatting on promo price in product edit form, working on getting % next to promo value, currently resides above it

--- app/assets/javascripts/promotion.js
+++ app/assets/javascripts/promotion.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  var $existingPromotionValue = $('#existing-promotion-value').text();   //hackity-hack
+  var $existingPromotionValue = $('#existing-promotion-value').text();       //hackity-hack
   // var $fullPrice = $('#full-price').text();
-  $('#existing-promotion-value').css('opacity', 0);                     //hackity-hack
+  $('#existing-promotion-value').css('opacity', 0);                        //hackity-hack
   $('#full-price').css('opacity', 0);                                     //hackity-hack
 
   $( "#slider-range-min" ).slider({
@@ -13,17 +13,20 @@ $(document).ready(function() {
       $( "#amount" ).val( ui.value );
       var $sliderValue = +$('#amount').val();
       var $fullPrice = +$('#full-price').text();
-      $('#promotion-price').text('Promotional Price: ' + ($fullPrice - (($sliderValue/100) * $fullPrice)));
+      $('#promotion-price').text('Promotional Price: $' + (($fullPrice - (($sliderValue/100) * $fullPrice))).toFixed(2));
     }
   });
   $( "#amount" ).val($( "#slider-range-min" ).slider( "value" ));
 
+  $('#amount').css('width', '50px')
+  $('#amount').prepend("<span>$</span>")
+  $('#promotion-price').css('margin-top', '10px');
 
   var $fullPrice = +$('#full-price').text();
-  $('#promotion-price').text('Promotional Price: ' + ($fullPrice - ((+$existingPromotionValue/100) * $fullPrice)));
+  $('#promotion-price').text('Promotional Price: $' + ($fullPrice - (((+$existingPromotionValue/100) * $fullPrice))).toFixed(2));
   $('#amount').keyup(function() {
     var $newPromotionValue = +$(this).val();
-    $('#promotion-price').text('Promotional Price: ' + (+$fullPrice - +(($newPromotionValue/100) * $fullPrice)));
+    $('#promotion-price').text('Promotional Price: $' + (+$fullPrice - +((($newPromotionValue/100) * $fullPrice))).toFixed(2));
   });
 });
 // ($newPromotionValue/100) * $fullPrice
