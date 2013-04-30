d5de7c8c98089b635624577587935f2a8e716e3e
Christopher <ChrisKnight.mail@gmail.com>
2013-04-30 10:20:56 -0600

    all products are updateable for a category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -22,6 +22,7 @@ class Admin::CategoriesController < ApplicationController
     if @category.save
       @category.promotion = promo
       @category.save
+
       redirect_to store_admin_categories_path,
         :notice => "Successfully created category."
     else
@@ -39,6 +40,11 @@ class Admin::CategoriesController < ApplicationController
     if @category.update_attributes(params[:category])
       @category.promotion = promo
       @category.save
+      @promo_products = Category.find_by_title(params[:category][:title]).products
+      @promo_products.each do |p|
+        p.promotion = Category.find_by_title(params[:category][:title]).promotion
+        p.save!
+      end
       redirect_to store_admin_categories_path,
       :notice  => "Successfully updated category."
     else
--- spec/features/admin_creates_category_promotion_spec.rb
+++ spec/features/admin_creates_category_promotion_spec.rb
@@ -2,7 +2,7 @@ require 'feature_spec_helper'
 
 describe "As an logged in Administrator of a store" do
   context "and I am in the admin dashboard" do
-    #before(:each) do
+    # before(:each) do
     #  admin = FactoryGirl.create(:user)
     #  @store = FactoryGirl.create(:store)
     #  @category = FactoryGirl.create(:category, store_id: @store)
@@ -11,25 +11,25 @@ describe "As an logged in Administrator of a store" do
     #  fill_in 'sessions_email', with: 'brad@example.com'
     #  fill_in 'sessions_password', with: 'password'
     #  click_button 'Login'
-    #end
-    #
-    #it "has a link to the promotion dashboard page" do
+    # end
+    
+    # it "has a link to the promotion dashboard page" do
     #  visit store_admin_manage_path(@store)
     #  page.should have_content "Categories"
-    #end
-    #
-    #it "when i click Categories, I should see a new category promo button" do
+    # end
+    
+    # it "when i click Categories, I should see a new category promo button" do
     #  visit store_admin_manage_path(@store)
     #  click_link "New Category Promotion"
     #  page.should have_content("Promotion Title")
-    #end
-    #
-    #it "I am able to set up a promotion for a product" do
+    # end
+    
+    # it "I am able to set up a promotion for a product" do
     #  pending
     #  visit store_admin_promotions_path(@store)
     #  click_button "Promote Product"
     #  page.should have_content("Product Promotion Form")
-    #end
+    # end
 
     it 'allows me to create a new promotion for a category', js: true do
       admin = FactoryGirl.create(:random_user)
