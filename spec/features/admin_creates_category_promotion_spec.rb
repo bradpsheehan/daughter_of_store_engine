1454fa60a65c2cf1c320ce832073d6c602d09408
Lew Parker <parker@earthvectors.com>
2013-04-29 18:13:33 -0600

    started Category Promotions

--- spec/features/admin_creates_category_promotion_spec.rb
+++ spec/features/admin_creates_category_promotion_spec.rb
@@ -5,7 +5,7 @@ describe "As an logged in Administrator of a store" do
     before(:each) do
       admin = FactoryGirl.create(:user)
       @store = FactoryGirl.create(:store)
-      @category = FactoryGirl.create(:category, store: @store)
+      @category = FactoryGirl.create(:category, store_id: @store)
       Role.promote(admin, @store, 'admin')
       visit login_path
       fill_in 'sessions_email', with: 'brad@example.com'
@@ -15,21 +15,41 @@ describe "As an logged in Administrator of a store" do
 
     it "has a link to the promotion dashboard page" do
       visit store_admin_manage_path(@store)
-      page.should have_content "categories"
+      page.should have_content "Categories"
     end
 
-    it "when i click promotions, I should be at promos dashboard" do
+    it "when i click Categories, I should see a new category promo button" do
       visit store_admin_manage_path(@store)
-      click_link "Promotions"
-      page.should have_content("Promote Product")
-      page.should have_content("Promote Category")
-      page.should have_content("Promote Store")
+      click_link "New Category Promotion"
+      page.should have_content("Promotion Title")
     end
 
     it "I am able to set up a promotion for a product" do
+      pending
       visit store_admin_promotions_path(@store)
       click_button "Promote Product"
       page.should have_content("Product Promotion Form")
+    end
+
+    it 'sllows me to craete a new promotion for a category' do
+      admin = FactoryGirl.create(:user)
+      @store = FactoryGirl.create(:store)
+      @category = FactoryGirl.create(:category, store_id: @store)
+      Role.promote(admin, @store, 'admin')
+      visit login_path
+      fill_in 'sessions_email', with: 'brad@example.com'
+      fill_in 'sessions_password', with: 'password'
+      click_button 'Login'
+      visit store_admin_manage_path(@store)
+
+      click_link "New Category Promotion"
+      page.should have_content("Promotion Title")
+
+      fill_in :promotion, with: 10.0
+      
+      click_button, "Submit"
+
+
     end
   end
 end
