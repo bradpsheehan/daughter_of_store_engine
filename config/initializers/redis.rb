821717162227b52785bc5af6370c2a1ae60d1108
xacaxulu <denmanjd@gmail.com>
2013-06-05 00:31:53 -0600

    test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# uri =Redis.connect(:url => ENV['REDISTOGO_URL']) 
-# Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+uri = URI.parse("redis://redistogo:eb5a38c51510d22f3574b6e098f55ffb@cod.redistogo.com:9563/")
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
